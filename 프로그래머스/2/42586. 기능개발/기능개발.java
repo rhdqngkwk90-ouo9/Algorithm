9a8b628b206825c85be3b3a999bbff9c23d0c410
rhdqngkwk90-ouo9 <ouorhdqngkwk90@gmail.com>
2025-01-24 23:47:52 +0900

    [level 2] Title: 기능개발, Time: 3.48 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    
+    static int time, day;
+    
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++) {
+            time = 0; day = 0;
+            time = 100 - progresses[i];
+            day = (time/speeds[i]);
+            if(time%speeds[i] > 0) day += 1;
+            q.add(day);
+        }
+        System.out.println(q);
+
+        List<Integer> list = new LinkedList<>();
+
+        while(!q.isEmpty()){
+            int value = q.poll(); //poll은 꺼내는 거
+            int cnt = 1;
+            
+            while(!q.isEmpty() && value >= q.peek()) { //peek은 값 확인하는 거
+                cnt++;
+                q.poll();
+            }
+            list.add(cnt);
+        }
+        
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+
+        return answer;
+    }
+}
