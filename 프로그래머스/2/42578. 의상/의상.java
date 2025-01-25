71f44631d7716201b73a7c591ffb27d7dfb6b063
rhdqngkwk90-ouo9 <ouorhdqngkwk90@gmail.com>
2025-01-25 23:43:33 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -5,14 +5,29 @@ class Solution {
     static Map<String, Integer> map = new HashMap<>();
 
     public int solution(String[][] clothes) {
+        
         int answer = 1; 
+        
         for(String[] arr : clothes){
-            map.put(arr[1], map.getOrDefault(arr[1], 1) + 1);
+            //map.put(arr[1], map.getOrDefault(arr[1], 1) + 1);
+            
+            // getOrDefault
+            String category = arr[1];
+            if (map.containsKey(category)) {
+                map.put(category, map.get(category) + 1); 
+            } else {
+                map.put(category, 1);  
+            }
+            
         }
-        for(String str : map.keySet()){
-            int num = map.get(str);
-
-            answer *= num;
+        
+        // getOrDefault
+        //for(String str : map.keySet()){
+        //    answer *= map.get(str);
+        //}
+        
+        for (int str : map.values()) {
+            answer *= (str + 1); 
         }
         
         return answer-1;
