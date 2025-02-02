5f456d1346a1359e68bc9d7d61ad7e07c0c0a658
rhdqngkwk90-ouo9 <ouorhdqngkwk90@gmail.com>
2025-02-03 07:12:20 +0900

    [level 2] Title: 피보나치 수, Time: 2.43 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = 1234567;
+        
+        int[] dp = new int[n + 1]; 
+        dp[0] = 0;  
+        dp[1] = 1;  
+
+        for (int i=2; i<=n; i++) {
+            dp[i] = ( dp[i-1] + dp[i-2] ) % num;
+        }
+
+        answer = dp[n];
+
+        return answer;
+    }
+}
